a1160fdd20ad3953ccd4e212995cdf17f25bfb71
Jared Matta <cherokeedreemer@gmail.com>
2024-06-07 13:12:06 -0600

    fix this project

--- YTDownloader/ytDownloader.py
+++ YTDownloader/ytDownloader.py
@@ -4,8 +4,8 @@ from sys import argv
 link = argv[1]
 yt = YouTube(link)
 
-print("Title: " yt.title)
-print("Views: " yt.views)
+print("Title: ", yt.title)
+print("Views: ", yt.views)
 
 yd = yt.streams.get_highest_resolution()
 
