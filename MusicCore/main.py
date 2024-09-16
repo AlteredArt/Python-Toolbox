ae380483f2ecb618d0c50795f92c946efe79e0c0
AlteredArt <cherokeedreemer@gmail.com>
2024-09-16 14:54:32 -0600

    cleaning up

---
+++ MusicCore/main.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+from sys import argv
+# import os
+
+yt = YouTube('https://www.youtube.com/watch?v=Zd_Wsjq2ljc')
+# title = yt.title
+# artists = yt.
+# print(title)
+
+# link = argv[1]
+# yt = YouTube(link)
+
+print("Title: ", yt.title)
+print("Views: ", yt.views)
+
+# yd = yt.streams.get_highest_resolution()
+
+# yd.download("/home/kalimax/Music/MusicDownloads")
--- yt.sh
+++
@@ -1,4 +0,0 @@
-#!bin/zsh
-
-cd /Users/path finish
-python3 ytDownloader.py $1
