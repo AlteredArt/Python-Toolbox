96bb44fe19d5efa1cf5089dfe8f30a84494aa672
AlteredArt <cherokeedreemer@gmail.com>
2024-09-19 22:07:55 -0600

    fix

--- MusicCore/main.py
+++ MusicCore/main.py
@@ -3,15 +3,20 @@ from sys import argv
 # import os
 
 yt = YouTube('https://www.youtube.com/watch?v=Zd_Wsjq2ljc')
-# title = yt.title
-# artists = yt.
-# print(title)
+title = yt.title
+artists = yt.
+print(title)
 
-# link = argv[1]
-# yt = YouTube(link)
+
+link = argv[1]
+
+
+yt = YouTube(link)
+
 
 print("Title: ", yt.title)
 print("Views: ", yt.views)
+
 
 # yd = yt.streams.get_highest_resolution()
 
