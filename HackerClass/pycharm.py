c540b8e98a1fef50908ae912e7c0d4ea535495f0
AlteredArt <cherokeedreemer@gmail.com>
2024-09-29 15:22:53 -0600

    start scrappier

--- HackerClass/pycharm.py
+++ HackerClass/pycharm.py
@@ -1,6 +1,18 @@
-def DNA_strand(dna):
-    # code here
-    for i in dna:
-        print()
-
-DNA_strand("AAAA")
+def count_positives_sum_negatives(arr):
+    
+    if arr is None or arr is not arr:
+        return []
+    
+    positive = 0
+    negative = 0
+    final_numbers = []
+    
+    for num in arr:
+        if num > 0:
+            num + positive
+        elif num  < 0:
+            num + negative
+        
+    final_numbers.append(positive)
+    final_numbers.append(negative)    
+    return final_numbers
