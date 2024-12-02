1e05fb89d0bd5d5c3866c5d83d1c9e25f69059b4
AlteredArt <cherokeedreemer@gmail.com>
2024-12-02 15:46:32 -0700

    add examples

--- pyexample.py
+++ pyexample.py
@@ -1,1 +1,15 @@
-print("hello")
+def sizely(size):
+    counter = 0 
+    finalString = []
+
+    while size >= counter:
+        finalString.append(1)
+        
+        while size >= counter:
+            finalString.append(0)
+
+        counter += 1
+    
+    print(finalString)
+
+sizely(9)
